a9a986737501657a67ab379abe757fcbef0c42e7
LuciferDeveloper <103754923+LuciferDeveloper@users.noreply.github.com>
2022-04-19 15:02:39 +0200

    Update shuffle.py

--- YukkiMusic/plugins/admins/shuffle.py
+++ YukkiMusic/plugins/admins/shuffle.py
@@ -1,50 +1,112 @@
 #
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This ile is part o < https://github.com/TeamYukki/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
-import random
 
-from pyrogram import filters
-from pyrogram.types import Message
+# Commands in Every Language
 
-from config import BANNED_USERS
-from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import db
-from YukkiMusic.utils.decorators import AdminRightsCheck
+# Bot Commands
+PING_COMMAND : ["ping", "بينج"]
+START_COMMAND : ["start"]
+HELP_COMMAND : ["help", "مساعده", "مساعدة"]
+SETTINGS_COMMAND : ["settings", "setting", "الاعدادات"]
+RELOAD_COMMAND : ["admincache", "reload", "ريسارت"]
+GSTATS_COMMAND : ["gstats", "احصائيات"]
+STATS_COMMAND : ["stats", "الاحصائيات"]
+LANGUAGE_COMMAND : ["lang", "language", "langs", "اللغه", "اللغة"]
 
-# Commands
-SHUFFLE_COMMAND = get_command("SHULE_COMMAND")
+# Play Commands
+PLAY_COMMAND : ["play", "vplay", "cplay", "cvplay", "playorce", "vplayorce", "cplayorce", "cvplayorce", "تشغيل", "شغل"]
+PLAYMODE_COMMAND : ["playmode", "التشغيل"]
+CHANNELPLAY_COMMAND : ["channelplay", "التشغيل2"]
+STREAM_COMMAND : ["stream", "cstream", "streamorce", "تشغيل فيديو"]
+
+#Playlists Commands
+PLAYLIST_COMMAND : ["playlist", "القائمه"]
+DELETEPLAYLIST_COMMAND : ["deleteplaylist", "مسح القائمه"]
 
 
-@app.on_message(
-    filters.command(SHUFFLE_COMMAND)
-    & filters.group
-    & ~filters.edited
-    & ~BANNED_USERS
-)
-@AdminRightsCheck
-async def admins(Client, message: Message, _, chat_id):
-    if not len(message.command) == 1:
-        return await message.reply_text(_["general_2"])
-    check = db.get(chat_id)
-    if not check:
-        return await message.reply_text(_["admin_21"])
-    try:
-        popped = check.pop(0)
-    except:
-        return await message.reply_text(_["admin_22"])
-    check = db.get(chat_id)
-    if not check:
-        check.insert(0, popped)
-        return await message.reply_text(_["admin_22"])
-    random.shuffle(check)
-    check.insert(0, popped)
-    await message.reply_text(
-        _["admin_23"].format(message.from_user.first_name)
-    )
+# Tools
+QUEUE_COMMAND : ["queue","cqueue", "player", "cplayer", "playing", "cplaying"]
+SONG_COMMAND : ["song", "video", "اغنيه", "فيديو"]
+LYRICS_COMMAND : ["lyrics", "الكلمات"]
+
+
+# Admin Commands
+AUTH_COMMAND : ["auth"]
+UNAUTH_COMMAND : ["unauth"]
+AUTHUSERS_COMMAND : ["authusers"]
+PAUSE_COMMAND : ["pause", "cpause", "اسكت"]
+RESUME_COMMAND : ["resume", "cresume", "كمل"]
+MUTE_COMMAND : ["mute", "cmute", "كتم"]
+UNMUTE_COMMAND : ["unmute", "cunmute", "اتكلم"]
+STOP_COMMAND : ["stop", "end", "cstop", "cend", "كفايه"]
+SKIP_COMMAND : ["skip", "cskip", "التالي"]
+SHULE_COMMAND : ["shule", "cshule", "خلط"]
+LOOP_COMMAND : ["loop", "cloop", "اعاده", "اعادة", "عيد"]
+SEEK_COMMAND : ["seek", "cseek", "seekback", "cseekback"]
+RESTART_COMMAND : ["restart", "اعاده تشغيل"]
+
+# Sudo Users Commaands
+
+# Sudo Command
+ADDSUDO_COMMAND : ["addsudo", "رفع مطور"]
+DELSUDO_COMMAND : ["delsudo", "تنزيل مطور"]
+SUDOUSERS_COMMAND : ["sudolist", "المطورين"]
+
+#Broadcast 
+BROADCAST_COMMAND : ["broadcast", "اذاعه"]
+
+#Blacklist chat
+BLACKLISTCHAT_COMMAND : ["blacklistchat"]
+WHITELISTCHAT_COMMAND : ["whitelistchat"]
+BLACKLISTEDCHAT_COMMAND : ["blacklistedchat"]
+
+# Video Calls Command
+VIDEOLIMIT_COMMAND : ["set_video_limit"]
+VIDEOMODE_COMMAND : ["videomode"]
+
+#Mantenance
+MAINTENANCE_COMMAND : ["maintenance", "صيانه"]
+LOGGER_COMMAND : ["logger"]
+
+#Heroku
+GETLOG_COMMAND : ["get_log"]
+GETVAR_COMMAND : ["get_var"]
+DELVAR_COMMAND : ["del_var"]
+SETVAR_COMMAND : ["set_var"]
+USAGE_COMMAND : ["usage"]
+VARS_COMMAND : ["vars", "conig"]
+
+
+#Basic Bot Commands
+UPDATE_COMMAND : ["update", "تحديث"]
+REBOOT_COMMAND : ["reboot"]
+AUTOEND_COMMAND : ["autoend"]
+
+#Private Bot Mode
+AUTHORIZE_COMMAND : ["authorize"]
+UNAUTHORIZE_COMMAND : ["unauthorize"]
+AUTHORIZED_COMMAND : ["authorized"]
+
+#Block Users Command
+BLOCK_COMMAND : ["block"]
+UNBLOCK_COMMAND : ["unblock"]
+BLOCKED_COMMAND : ["blockedusers"]
+
+#sPEEDTEST
+SPEEDTEST_COMMAND : ["speedtest"]
+
+#Voice chat commands
+ACTIVEVC_COMMAND : ["activevoice"]
+ACTIVEVIDEO_COMMAND : ["activevideo"]
+
+#Gban Commands
+GBAN_COMMAND : ["gban"]
+UNGBAN_COMMAND : ["ungban"]
+GBANNED_COMMAND : ["gbannedusers"]
